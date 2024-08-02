6e913f17db912ee7a5422eece67af31633f2b80a
gayunn <83623554+gayun66@users.noreply.github.com>
2024-08-02 10:41:30 +0900

    components test

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+const sass = require("sass");
+const path = require("path");
+
+module.exports = function(eleventyConfig) {
+  // Sass 컴파일 설정
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function(inputContent, inputPath) {
+      let result = sass.compile(inputPath);
+      return async (data) => {
+        return result.css;
+      };
+    }
+  });
+
+  // CSS 파일 복사
+  eleventyConfig.addPassthroughCopy("_includes/styles");
+  // JavaScript 파일 복사
+  // eleventyConfig.addPassthroughCopy("src/includes/js");
+  // 다른 정적 파일들도 필요하다면 여기에 추가
+  // eleventyConfig.addPassthroughCopy("src/images");
+
+  return {
+    dir: {
+      // input: "src",
+      output: "docs",
+      includes: "_includes",
+    }
+  };
+};
